821d0e1909f01feb4152a26a74a5b64053abd227
jstenger <jake.stenger.19@bishops.com>
2020-02-24 11:49:12 -0800

    fixed Karplus audio glitch with frame-to-frame rollover

--- docs/karplus-strong-processor.js
+++ docs/karplus-strong-processor.js
@@ -1,12 +1,16 @@
 class KarplusStrongProcessor extends AudioWorkletProcessor {
+
+    prev = 0; // last sample from previous 128 sample batch
+
     process(inputs, outputs, parameters) {
         const input = inputs[0][0]; // input 0, channel 0
         const output = outputs[0][0];
-        var prev = 0;
+        
         for(let i = 0; i < input.length; i++) {
-            output[i] = (input[i] + prev) / 2;
-            prev = input[i];
+            output[i] = (input[i] + this.prev) / 2;
+            this.prev = input[i];
         }
+        this.prev = output[output.length - 1];
         return true;
     }
 }
