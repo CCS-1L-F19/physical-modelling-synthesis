0dab313c60ea65249c4e7583eb3c9d5159cfc842
jstenger <jake.stenger.19@bishops.com>
2020-02-24 11:06:38 -0800

    working Karplus-Strong algorithm (works only in secure contexts)

--- docs/karplus-strong-processor.js
+++ docs/karplus-strong-processor.js
@@ -3,7 +3,7 @@ class KarplusStrongProcessor extends AudioWorkletProcessor {
         const input = inputs[0][0]; // input 0, channel 0
         const output = outputs[0][0];
         var prev = 0;
-        for(let i = 0; i < input.length(); i++) {
+        for(let i = 0; i < input.length; i++) {
             output[i] = (input[i] + prev) / 2;
             prev = input[i];
         }
