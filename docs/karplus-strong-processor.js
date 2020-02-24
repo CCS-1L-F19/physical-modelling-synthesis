8dd28885b32e8e061984bc95f9fa0a9c02cbb167
jstenger <jake.stenger.19@bishops.com>
2020-02-23 23:25:18 -0800

    implemented Karplus-Strong averaging algorithm in an AudioWorketProcessor

---
+++ docs/karplus-strong-processor.js
@@ -0,0 +1,14 @@
+class KarplusStrongProcessor extends AudioWorkletProcessor {
+    process(inputs, outputs, parameters) {
+        const input = inputs[0][0]; // input 0, channel 0
+        const output = outputs[0][0];
+        var prev = 0;
+        for(let i = 0; i < input.length(); i++) {
+            output[i] = (input[i] + prev) / 2;
+            prev = input[i];
+        }
+        return true;
+    }
+}
+  
+registerProcessor('karplus-strong-processor', KarplusStrongProcessor)
